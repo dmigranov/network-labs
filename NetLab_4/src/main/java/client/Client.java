ef10afce618d37185ec7f49dd75395496317a466
Денис  Мигранов <d.migranov@g.nsu.ru>
2018-11-05 20:14:39 +0700

    handler is better now

--- NetLab_4/src/main/java/client/Client.java
+++ NetLab_4/src/main/java/client/Client.java
@@ -1,8 +1,10 @@
 package client;
 
-public class Client {
-    public static void main()
-    {
 
+
+public class Client {
+    public static void main(String[] args) {
+        String s = "users/fsd";
+        System.out.println(s.matches(("users/(.+)")));
     }
 }
--- NetLab_4/src/main/java/server/RestHandler.java
+++ NetLab_4/src/main/java/server/RestHandler.java
@@ -5,20 +5,20 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
-import java.net.URI;
+import java.io.InputStream;
 
 public class RestHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
         String method = exchange.getRequestMethod();        //POST
-        Headers responseHeaders = exchange.getResponseHeaders();
-
+        Headers reqHeaders = exchange.getRequestHeaders();
+        InputStream reqBody = exchange.getRequestBody();
         String path = exchange.getRequestURI().getPath();   //login etc.
         if(method.equals("POST"))
         {
             if(path.equals("/login"))
             {
-                System.out.println("login");
+                //System.out.println(reqHeaders.get("Content-Type").get(0)); //TODO: проверка на json
                 //TODO: parse body!
             }
             else if (path.equals("/logout"))
@@ -30,7 +30,22 @@ public class RestHandler implements HttpHandler {
                 exchange.sendResponseHeaders(400, -1);
             }
         }
-
+        else if(method.equals("GET"))
+        {
+            if (path.equals("/users"))
+            {
+                System.out.println("get users"); //TODO: проверка на json
+                //TODO: parse body!
+            }
+            else if (path.matches("/users/(.+)"))
+            {
+                System.out.println(path);
+            }
+            else
+            {
+                exchange.sendResponseHeaders(400, -1);
+            }
+        }
         //System.out.println(exchange.getRequestHeaders());
     }
 }
