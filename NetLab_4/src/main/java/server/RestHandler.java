30efb58d4f0993677e051fb2b9f0905a4ecab0ba
Денис  Мигранов <d.migranov@g.nsu.ru>
2018-11-09 14:09:16 +0700

    not good it reads request befor body and when body comes there is no thread...

--- NetLab_4/src/main/java/server/RestHandler.java
+++ NetLab_4/src/main/java/server/RestHandler.java
@@ -1,87 +1,63 @@
 package server;
 
-import io.undertow.Undertow;
+
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.util.HeaderMap;
 import io.undertow.util.HttpString;
-import org.xnio.channels.StreamSourceChannel;
+
 //import org.json.*;
+import org.xnio.streams.ChannelInputStream;
+import org.apache.commons.io.IOUtils;
 
 import java.io.IOException;
-import java.io.InputStream;
+import java.nio.*;
+import java.nio.charset.StandardCharsets;
+
 
 public class RestHandler implements HttpHandler {
-    /*@Override
-    public void handle(HttpExchange exchange) throws IOException {
-        String method = exchange.getRequestMethod();        //POST
-        Headers reqHeaders = exchange.getRequestHeaders();
-        InputStream reqBody = exchange.getRequestBody();
-        String path = exchange.getRequestURI().getPath();   //login etc.
-        if(method.equals("POST"))
-        {
-            if(path.equals("/login"))
-            {
-                //System.out.println(reqHeaders.get("Content-Type").get(0)); //TODO: проверка на json
-                //TODO: parse body!
-            }
-            else if (path.equals("/logout"))
-            {
-                System.out.println("logout");
-            }
-            else
-            {
-                exchange.sendResponseHeaders(400, -1);
-            }
-        }
-        else if(method.equals("GET"))
-        {
-            if (path.equals("/users"))
-            {
-                System.out.println("get users"); //TODO: проверка на json
-                //TODO: parse body!
-            }
-            else if (path.matches("/users/(.+)"))
-            {
-                System.out.println(path);
-            }
-            else
-            {
-                exchange.sendResponseHeaders(400, -1);
-            }
-        }
-        //System.out.println(exchange.getRequestHeaders());
-    }*/
-
     @Override
-    public void handleRequest(HttpServerExchange exchange) throws Exception {
+    public void handleRequest(HttpServerExchange exchange) //throws Exception
+    {
         String method = exchange.getRequestMethod().toString();        //POST
         HeaderMap headers = exchange.getRequestHeaders();
         String path = exchange.getRequestURI();
-        StreamSourceChannel body;
-        //body = exchange.getRequestChannel() //if there is no req body, calling this method may cause the next req to be processed. NB: close()!
-
-        System.out.println(method + " " + path + " " + headers.size());
+        ChannelInputStream bodyStream = new ChannelInputStream(exchange.getRequestChannel()); //if there is no req body, calling this method may cause the next req to be processed. NB: close()!
+        //String body = new BufferedReader(new InputStreamReader(bodyStream)).lines().collect(Collectors.joining("\n"));
+        String body = null;
+        try {
+            System.out.println("i'm here");
+            body = IOUtils.toString(bodyStream, "UTF8");
+        }
+        catch(IOException e)
+        {
+            //TODOclose stream?
+            exchange.setStatusCode(500);
+        }
+        System.out.println(body);
 
         if(method.equals("POST"))
         {
-            if(path.equals("/login"))
-            {
-                System.out.println("login");
-                System.out.println(headers.get("Content-Type").get(0)); //TODO: проверка на json
-                //TODO: parse body!
-            }
-            else if (path.equals("/logout"))
-            {
-                System.out.println("logout");
-            }
-            else if(path.equals("/messages"))
-            {
-                System.out.println("messages");
-            }
-            else
-            {
-                exchange.setStatusCode(405);
+            switch (path) {
+                case "/login":
+                    System.out.println("login");
+                    if (headers.get("Content-Type") != null && headers.get("Content-Type").get(0).equals("application/json")) {
+                        System.out.println("JSON"); //TODO: проверка на json
+                        //TODO: parse body!
+
+                    }
+                    else
+                        exchange.setStatusCode(400);
+                    break;
+                case "/logout":
+                    System.out.println("logout");
+                    break;
+                case "/messages":
+                    System.out.println("messages");
+                    break;
+                default:
+                    exchange.setStatusCode(405);
+                    break;
             }
         }
         else if(method.equals("GET"))
