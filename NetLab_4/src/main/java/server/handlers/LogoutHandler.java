97fb665e50c0f56c739d2081792a1165b6dc4963
Денис  Мигранов <d.migranov@g.nsu.ru>
2018-11-19 22:27:07 +0700

    logout is even better now!

--- NetLab_4/src/main/java/server/handlers/LogoutHandler.java
+++ NetLab_4/src/main/java/server/handlers/LogoutHandler.java
@@ -5,7 +5,6 @@ import io.undertow.util.HeaderMap;
 import io.undertow.util.HeaderValues;
 import io.undertow.util.Headers;
 import org.json.JSONObject;
-import org.xnio.streams.ChannelInputStream;
 import org.xnio.streams.ChannelOutputStream;
 import server.Message;
 import server.Messages;
@@ -24,25 +23,22 @@ public class LogoutHandler extends AbstractRestHandler {
         HeaderMap requestHeaders = exchange.getRequestHeaders();
         HeaderMap responseHeaders = exchange.getResponseHeaders();
         ChannelOutputStream responseStream;
-        try (ChannelInputStream bodyStream = new ChannelInputStream(exchange.getRequestChannel()))
-        {
-            HeaderValues authorizationHeader;
-            if ((authorizationHeader = requestHeaders.get(Headers.AUTHORIZATION)) != null)
-            {
-                User user = users.get(authorizationHeader.get(0).substring(6));
-                responseHeaders.add(Headers.CONTENT_TYPE, "application/json");
-                JSONObject respObject = new JSONObject();
-                respObject.put("message", "bye!");
-                byte[] jsonBytes = respObject.toString().getBytes(StandardCharsets.UTF_8);
-                responseStream = new ChannelOutputStream(exchange.getResponseChannel());
-                responseStream.write(jsonBytes);
-                responseStream.close();
-                user.setOnline(false);
-                messages.add(new Message(user.getUsername() + " left", -1));
-            }
-            else
-                exchange.setStatusCode(400);
-        }
 
+        HeaderValues authorizationHeader;
+        if ((authorizationHeader = requestHeaders.get(Headers.AUTHORIZATION)) != null)
+        {
+            User user = users.get(authorizationHeader.get(0).substring(6));
+            responseHeaders.add(Headers.CONTENT_TYPE, "application/json");
+            JSONObject respObject = new JSONObject();
+            respObject.put("message", "bye!");
+            byte[] jsonBytes = respObject.toString().getBytes(StandardCharsets.UTF_8);
+            responseStream = new ChannelOutputStream(exchange.getResponseChannel());
+            responseStream.write(jsonBytes);
+            responseStream.close();
+            user.setOnline(false);
+            messages.add(new Message(user.getUsername() + " left", -1));
+        }
+        else
+            exchange.setStatusCode(400);
     }
 }
