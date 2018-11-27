ab19f52c77510410649a71a44a482bdb8572a378
Денис  Мигранов <d.migranov@g.nsu.ru>
2018-11-27 22:23:29 +0700

    that's it for today

--- NetLab_6/src/ru/nsu/migranov/portforwarder/Main.java
+++ NetLab_6/src/ru/nsu/migranov/portforwarder/Main.java
@@ -15,7 +15,7 @@ public class Main {
         InetAddress rhost = null;
         try
         {
-            rhost = InetAddress.getByName(args[1]);
+            rhost = InetAddress.getByName(args[1]); //надо зарезолвить
         }
         catch(java.net.UnknownHostException e)
         {
--- NetLab_6/src/ru/nsu/migranov/portforwarder/PortForwarder.java
+++ NetLab_6/src/ru/nsu/migranov/portforwarder/PortForwarder.java
@@ -1,6 +1,14 @@
 package ru.nsu.migranov.portforwarder;
 
+import java.io.IOException;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.util.Iterator;
+import java.util.Set;
 
 public class PortForwarder {
     private int lport;
@@ -11,5 +19,37 @@ public class PortForwarder {
         this.rhost = rhost;
         this.rport = rport;
     }
+
+
+    public void run()
+    {
+        try(Selector selector = Selector.open();
+            ServerSocketChannel ssc = ServerSocketChannel.open();) {
+            ssc.bind(new InetSocketAddress("localhost", lport)); //название протокола?
+            ssc.configureBlocking(false);
+            ssc.register(selector, SelectionKey.OP_ACCEPT);
+            ByteBuffer buf = ByteBuffer.allocate(1024);
+
+            while(true)
+            {
+                selector.select(); //возвращает только если хотя бы один channel выбран
+                Set<SelectionKey> selectedKeys = selector.selectedKeys();
+                Iterator<SelectionKey> iter = selectedKeys.iterator();
+                while(iter.hasNext())
+                {
+                    System.out.println("HERE");
+                }
+            }
+
+        }
+        catch(IOException e)
+        {
+            System.err.println("IOException!");
+            System.exit(2);
+        }
+
+    }
+
+
 
 }
