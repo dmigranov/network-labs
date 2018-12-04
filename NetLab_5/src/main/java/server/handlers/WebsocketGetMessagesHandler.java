142cf6280434e1487e2347d81954bd947472e01a
Денис  Мигранов <d.migranov@g.nsu.ru>
2018-12-04 20:45:13 +0700

    1. Now I can detect closing of a connection! 2. I've a problem: using sleep in one threads preserves another thread from getting ANYTHING! Nani?

--- NetLab_5/src/main/java/server/handlers/WebsocketGetMessagesHandler.java
+++ NetLab_5/src/main/java/server/handlers/WebsocketGetMessagesHandler.java
@@ -1,18 +1,17 @@
 package server.handlers;
 
-import io.undertow.websockets.WebSocketConnectionCallback;
+import io.undertow.Handlers;
+import io.undertow.server.HttpServerExchange;
+import server.Messages;
+import server.Users;
 
-import io.undertow.websockets.core.WebSocketChannel;
-import io.undertow.websockets.core.WebSockets;
-import io.undertow.websockets.spi.WebSocketHttpExchange;
-
-public class WebsocketGetMessagesHandler implements WebSocketConnectionCallback {
-
+public class WebsocketGetMessagesHandler extends AbstractRestHandler {
+    public WebsocketGetMessagesHandler(Users users, Messages messages) {
+        super(users, messages);
+    }
 
     @Override
-    public void onConnect(WebSocketHttpExchange exchange, WebSocketChannel webSocketChannel) {
-        System.out.println("YO");
-        while(true)
-            WebSockets.sendText("HELLO", webSocketChannel, null);
+    public void handleRequest(HttpServerExchange exchange) throws Exception {
+        Handlers.websocket(new WebsocketGetMessagesCallback()).handleRequest(exchange);
     }
 }
