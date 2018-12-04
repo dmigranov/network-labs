1f5c1d7b216e491f5418fd10d5f20d1c1e07f1d5
Денис  Мигранов <d.migranov@g.nsu.ru>
2018-12-04 21:22:30 +0700

    the problem is still there

--- NetLab_5/src/main/java/server/handlers/WebsocketGetMessagesCallback.java
+++ NetLab_5/src/main/java/server/handlers/WebsocketGetMessagesCallback.java
@@ -1,18 +1,39 @@
 package server.handlers;
 
+import io.undertow.server.protocol.framed.AbstractFramedChannel;
 import io.undertow.websockets.WebSocketConnectionCallback;
 
 import io.undertow.websockets.core.WebSocketChannel;
 import io.undertow.websockets.core.WebSockets;
 import io.undertow.websockets.spi.WebSocketHttpExchange;
+import org.xnio.ChannelListener;
 
 public class WebsocketGetMessagesCallback implements WebSocketConnectionCallback {
 
-
+    boolean hasToStop = false;
     @Override
     public void onConnect(WebSocketHttpExchange exchange, WebSocketChannel webSocketChannel) {
-        System.out.println("YO");
-        while(true)
-            WebSockets.sendText("HELLO", webSocketChannel, null);
+
+        webSocketChannel.getCloseSetter().set(new ChannelListener<AbstractFramedChannel>() {
+            @Override
+            public void handleEvent(AbstractFramedChannel abstractFramedChannel) {
+                System.out.println("CLOSE!");
+                hasToStop = true;
+            }
+        });
+
+
+        int t = new java.util.Random().nextInt();
+        //System.out.println(t);
+        while(!hasToStop) {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("YO" + t);
+            WebSockets.sendText("START", webSocketChannel, null);
+        }
     }
 }
