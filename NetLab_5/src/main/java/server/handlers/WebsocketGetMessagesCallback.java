8eeebf5ae9025da98760108c1d4d9b76511d841b
Денис  Мигранов <d.migranov@g.nsu.ru>
2018-12-04 20:16:37 +0700

    less pornography now :) the code is better

---
+++ NetLab_5/src/main/java/server/handlers/WebsocketGetMessagesCallback.java
@@ -0,0 +1,18 @@
+package server.handlers;
+
+import io.undertow.websockets.WebSocketConnectionCallback;
+
+import io.undertow.websockets.core.WebSocketChannel;
+import io.undertow.websockets.core.WebSockets;
+import io.undertow.websockets.spi.WebSocketHttpExchange;
+
+public class WebsocketGetMessagesCallback implements WebSocketConnectionCallback {
+
+
+    @Override
+    public void onConnect(WebSocketHttpExchange exchange, WebSocketChannel webSocketChannel) {
+        System.out.println("YO");
+        while(true)
+            WebSockets.sendText("HELLO", webSocketChannel, null);
+    }
+}
